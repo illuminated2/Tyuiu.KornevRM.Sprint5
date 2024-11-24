d7398343444b40369c2fc5d1e9ce6a3b23cbdd3e
illuminated <iLLuminated_89@mail.ru>
2024-11-24 17:21:42 +0500

    Task2

--- Tyuiu.KornevRM.Sprint5.Task2.V29.Test/DataServiceTest.cs
+++ Tyuiu.KornevRM.Sprint5.Task2.V29.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KornevRM.Sprint5.Task2.V29.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
             FileInfo fileInfo = new FileInfo(path);
             bool Exists = fileInfo.Exists;
             Assert.IsTrue(Exists);
