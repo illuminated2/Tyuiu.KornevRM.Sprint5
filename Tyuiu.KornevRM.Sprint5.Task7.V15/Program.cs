897d955fbb6156a91f5fd4b6fc5c053e1ae24cb6
illuminated <iLLuminated_89@mail.ru>
2024-11-28 13:50:37 +0500

    Task7

--- Tyuiu.KornevRM.Sprint5.Task7.V15/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task7.V15/Program.cs
@@ -6,8 +6,21 @@ namespace Tyuiu.KornevRM.Sprint5.Task7.V15
     {
         static void Main(string[] args)
         {
-            DataService ds = new DataService();
 
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р.М.  |  ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р.М. | ИСПб-24-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл InPutDataFileTask7V15.txt в котором есть набор символьных      *");
+            Console.WriteLine("* данных.  Заменить все заглавные латинские буквы на строчные.            *");
+            Console.WriteLine("* Полученный результат сохранить в файл OutPutDataFileTask7V10.txt.       *");
+            Console.WriteLine("*                                                                         *");
             string path = @"C:\DataSprint5\InPutDataFileTask7V15.txt";
             string pathSaveFile = ds.LoadDataAndSave(path);
             Console.WriteLine("Данные находятся в файле: " + path);
