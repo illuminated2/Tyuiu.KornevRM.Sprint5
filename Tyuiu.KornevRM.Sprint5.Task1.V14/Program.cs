2053e5844a26042c4671e6653382930e3db9c391
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:42:56 +0500

    Task1

--- Tyuiu.KornevRM.Sprint5.Task1.V14/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task1.V14/Program.cs
@@ -8,10 +8,22 @@ namespace Tyuiu.KornevRM.Sprint5.Task1.V14
             DataService ds = new DataService();
             int startValue = -5;
             int stopValue = -5;
-
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение и сохраняет             *");
+            Console.WriteLine("*  в текстовый файл OutPutFileTask1.txt                                   *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
+
 
             Console.WriteLine("startValue = " + startValue);
             Console.WriteLine("stopValue = " + stopValue);
