359512cae5e50a5f8b6dc98e88e07f99485c234d
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:45:19 +0500

    Task3

--- Tyuiu.KornevRM.Sprint5.Task3.V2/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task3.V2/Program.cs
@@ -6,7 +6,20 @@ namespace Tyuiu.KornevRM.Sprint5.Task3.V2
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-           
+
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение y = (e^x)/x вычислить его значение при x = 3, результат  *");
+            Console.WriteLine("* сохранить в бинарный файл OutPutFileTask3.bin и вывести на консоль.     *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
