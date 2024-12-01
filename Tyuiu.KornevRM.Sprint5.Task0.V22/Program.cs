1063c009ed13d20c0f7d58ac4d867a298440407a
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:40:56 +0500

    Task0

--- Tyuiu.KornevRM.Sprint5.Task0.V22/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task0.V22/Program.cs
@@ -7,6 +7,22 @@ namespace Tyuiu.KornevRM.Sprint5.Task0.V22
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+            Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль.              *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
             int x = 3;
             string res = ds.SaveToFileTextData(x);
             Console.WriteLine("x = " + x);
