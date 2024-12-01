9e8d2007044c73a17846958c0edc083952175f5d
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:49:23 +0500

    Task5

--- Tyuiu.KornevRM.Sprint5.Task5.V16/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task5.V16/Program.cs
@@ -6,7 +6,23 @@ namespace Tyuiu.KornevRM.Sprint5.Task5.V16
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл InPutDataFileTask5V24.txt в котором есть набор значений.       *");
+            Console.WriteLine("* Найти сумму всех двузначных целых чисел в файле.  Полученный            *");
+            Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить         *");
+            Console.WriteLine("* до трёх знаков после запятой.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
             string path = @"C:\DataSprint5\InPutDataFileTask5V16.txt";
             Console.WriteLine("Данные находятся в файле: " + path);
 
