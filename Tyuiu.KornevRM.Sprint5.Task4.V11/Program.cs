3c1fdfa0c33e2c164ed2dc98b7bfc07581a11b86
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:48:07 +0500

    Task4

--- Tyuiu.KornevRM.Sprint5.Task4.V11/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task4.V11/Program.cs
@@ -7,7 +7,22 @@ namespace Tyuiu.KornevRM.Sprint5.Task4.V11
         {
             DataService ds = new DataService();
             string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V11.txt";
-
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл InPutDataFileTask4V29.txt в котором есть вещественное значение *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле             *");
+            Console.WriteLine("* y = sinx + x^2/2 Округлить до трёх знаков после запятой.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
             Console.WriteLine("Данные находяться в файле: " + path);
 
             Console.WriteLine("***************************************************************************");
