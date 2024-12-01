464a1f22d1849c8a9a16f882fb186b0b7d4656e8
illuminated <iLLuminated_89@mail.ru>
2024-12-01 17:51:03 +0500

    Task6

--- Tyuiu.KornevRM.Sprint5.Task6.V10/Program.cs
+++ Tyuiu.KornevRM.Sprint5.Task6.V10/Program.cs
@@ -6,10 +6,21 @@ namespace Tyuiu.KornevRM.Sprint5.Task6.V10
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            Console.WriteLine("********************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
-            Console.WriteLine("*******************************************************************************");
-
+            Console.Title = "Спринт #5 | Выполнил: Корнев Р. М. | ИСПб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл InPutDataFileTask6V4.txt в котором есть набор символьных       *");
+            Console.WriteLine("* данных. Найти количество слов длиной четыре символа в заданной строке.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
             string path = @"C:\DataSprint5\InPutDataFileTask6V10.txt";
 
             Console.WriteLine("Данные находятся в файле" + path);
